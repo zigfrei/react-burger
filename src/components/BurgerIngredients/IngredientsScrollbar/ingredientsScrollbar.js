83aaf3a7b3ca608ebf7f0b1b492c81ef85b6f9a9
zigfrei <zigfrei.zf@gmail.com>
2022-02-13 16:09:50 +0300

    feat: making tab work again

--- src/components/BurgerIngredients/IngredientsScrollbar/ingredientsScrollbar.js
+++ src/components/BurgerIngredients/IngredientsScrollbar/ingredientsScrollbar.js
@@ -2,11 +2,36 @@ import ingredientsScrollbar from "./ingredientsScrollbar.module.css";
 import TemplateIngredient from "../TemplateIngredient/templateIngredient.js";
 import PropTypes from "prop-types";
 import { menuItemPropTypes } from "../../../utils/constants.js";
+import React from "react";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
 export default function IngredientsScrollbar({ ingredientsData }) {
+  const [current, setCurrent] = React.useState();
   const bun = [];
   const sauce = [];
   const filling = [];
+  const bunRef = React.useRef(null);
+  const sauceRef = React.useRef(null);
+  const fillingRef = React.useRef(null);
+  const bunHandleClick = (e) => {
+    setCurrent(e);
+    bunRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+  const sauceHandleClick = (e) => {
+    setCurrent(e);
+    sauceRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+  const fillingHandleClick = (e) => {
+    setCurrent(e);
+    fillingRef.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
   ingredientsData.forEach((ingredient) => {
     if (ingredient.type === "bun") {
       bun.push(ingredient);
@@ -17,26 +42,57 @@ export default function IngredientsScrollbar({ ingredientsData }) {
     }
   });
   return (
-    <ul className={ingredientsScrollbar.section}>
-      <h2 className="text text_type_main-medium mt-10 mb-6">Булки</h2>
-      <div className={`${ingredientsScrollbar.element} pl-4`}>
-        {bun.map((ingredient) => (
-          <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
-        ))}
-      </div>
-      <h2 className="text text_type_main-medium mt-10 mb-6">Соусы</h2>
-      <div className={`${ingredientsScrollbar.element} pl-4`}>
-        {sauce.map((ingredient) => (
-          <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
-        ))}
-      </div>
-      <h2 className="text text_type_main-medium mt-10 mb-6">Начинки</h2>
-      <div className={`${ingredientsScrollbar.element} pl-4`}>
-        {filling.map((ingredient) => (
-          <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
-        ))}
-      </div>
-    </ul>
+    <>
+      <nav className={`${ingredientsScrollbar.main} mt-5`}>
+        <Tab
+          value="Булки"
+          active={current === "Булки"}
+          onClick={(bunHandleClick)}
+        >
+          Булки
+        </Tab>
+        <Tab
+          value="Соусы"
+          active={current === "Соусы"}
+          onClick={(sauceHandleClick)}
+        >
+          Соусы
+        </Tab>
+        <Tab
+          value="Начинки"
+          active={current === "Начинки"}
+          onClick={(fillingHandleClick)}
+        >
+          Начинки
+        </Tab>
+      </nav>
+      <ul className={ingredientsScrollbar.scroll}>
+        <h2 ref={bunRef} className="text text_type_main-medium mt-10 mb-6">
+          Булки
+        </h2>
+        <div className={`${ingredientsScrollbar.element} pl-4`}>
+          {bun.map((ingredient) => (
+            <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
+          ))}
+        </div>
+        <h2 ref={sauceRef} className="text text_type_main-medium mt-10 mb-6">
+          Соусы
+        </h2>
+        <div className={`${ingredientsScrollbar.element} pl-4`}>
+          {sauce.map((ingredient) => (
+            <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
+          ))}
+        </div>
+        <h2 ref={fillingRef} className="text text_type_main-medium mt-10 mb-6">
+          Начинки
+        </h2>
+        <div className={`${ingredientsScrollbar.element} pl-4`}>
+          {filling.map((ingredient) => (
+            <TemplateIngredient key={ingredient._id} ingredient={ingredient} />
+          ))}
+        </div>
+      </ul>
+    </>
   );
 }
 IngredientsScrollbar.propTypes = {
--- src/components/BurgerIngredients/burgerIngredients.js
+++ src/components/BurgerIngredients/burgerIngredients.js
@@ -1,4 +1,3 @@
-import IngredientsTab from "./IngredientsTab/ingredientsTab.js";
 import IngredientsScrollbar from "./IngredientsScrollbar/ingredientsScrollbar.js";
 import PropTypes from "prop-types";
 import { menuItemPropTypes } from "../../utils/constants.js";
@@ -7,7 +6,6 @@ export default function BurgerIngredients({ data }) {
   return (
     <section className="mr-5">
       <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-      <IngredientsTab />
       <IngredientsScrollbar ingredientsData={data} />
     </section>
   );
